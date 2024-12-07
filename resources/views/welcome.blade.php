607d0d9393930a4168f437d959fc97cd34f4be8b
hungrysorrow <heiliglied@gmail.com>
2024-12-07 06:58:27 +0000

    라라벨 11 vue3 마이그레이션 완료

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.layout')
 
+@section('heads')
+<style>
+.titlebox {
+	width: 100%;
+	height: 100%;
+	display: table;
+}
+
+.welcome {
+	position: relative;
+	width: 320px;
+	height: 240px;
+	color: #cecece;
+	font-size: 36pt;
+	text-align: center;
+	vertical-align: middle;
+	display: table-cell;
+	top: -55px;
+}
+</style>
+@endsection
+
 @section('contents')
 <div class="wrapper bg-white">
 	<div class="navigation bg-aliceblue">
